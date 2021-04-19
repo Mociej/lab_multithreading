fb16a168c244802c025cce6e1a088287513d3587
Mociej <maciekkocemba@gmail.com>
2021-04-19 18:29:33 +0200

    init BetterRadar

---
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -0,0 +1,25 @@
+package edu.iis.mto.multithread;
+
+public class BetterRadar {
+    private static final int ROCKET_COUNT = 10;
+    private PatriotBattery battery;
+
+    public BetterRadar(PatriotBattery battery) {
+        this.battery = battery;
+    }
+
+    public void notice(Scud enemyMissle) {
+        launchPatriot(enemyMissle, ROCKET_COUNT);
+    }
+
+    private void launchPatriot(Scud enemyMissle, int rocketCount) {
+        Runnable launchPatriotTask = () -> {
+            for (int i = 0; i < rocketCount; i++) {
+                battery.launchPatriot(enemyMissle);
+            }
+        };
+
+        Thread launchingThread = new Thread(launchPatriotTask);
+        launchingThread.start();
+    }
+}
